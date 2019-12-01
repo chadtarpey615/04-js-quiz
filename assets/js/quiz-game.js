07f59f62cac14becdb0e3b74b294a7a51b383217
travis-john <travis.wl.john@gmail.com>
2019-12-01 15:05:03 -0500

    revised starter html, test dynamic css global js variables

---
+++ assets/js/quiz-game.js
@@ -0,0 +1,12 @@
+// global variables
+var start = document.getElementById("start-btn"),
+    quiz = document.getElementById("quiz-container"),
+    question = document.getElementById("question"),
+    qImg = document.getElementById("quiz-img"),
+    choiceA = document.getElementById("choice-a"),
+    choiceB = document.getElementById("choice-b"),
+    choiceC = document.getElementById("choice-c"),
+    counter = document.getElementById("counter"),
+    timeGauge = document.getElementById("timeGauge"),
+    progress = document.getElementById("progress"),
+    scoreDiv = document.getElementById("score-container");
